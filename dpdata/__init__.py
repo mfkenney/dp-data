f196329e03d41d4dc2477365f383b8aaf005014e
Michael Kenney <mfkenney@gmail.com>
2014-09-11 23:40:12 -0700

    Update package init file.

--- dpdata/__init__.py
+++ dpdata/__init__.py
@@ -1,1 +1,19 @@
 __version__ = '0.1'
+
+
+def expand_lists(data):
+    """
+    Given a dictionary of data values, return a new dictionary
+    with all list values expanded, e.g:
+
+      If data[name] == val where val is a list
+
+        name_0: val[0], name_1: val[1], ..., name_N: val[N]
+    """
+    newdict = data.copy()
+    for k, v in data.iteritems():
+        if isinstance(v, list) or isinstance(v, tuple):
+            d = dict([(k + '_' + str(i), e) for i, e in enumerate(v)])
+            del newdict[k]
+            newdict.update(d)
+    return newdict
