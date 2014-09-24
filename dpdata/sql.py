e2544add788f4bbcafc4ec955178ffafdbdf16f0
Michael Kenney <mfkenney@gmail.com>
2014-09-24 10:45:51 -0700

    Updated some function interfaces in sql module.

--- dpdata/sql.py
+++ dpdata/sql.py
@@ -7,6 +7,7 @@ import pandas as pd
 from sqlalchemy import Table, MetaData, select, Column, Integer,\
     Float, Text
 from sqlalchemy.sql import func
+from sqlalchemy.exc import NoSuchTableError
 from collections import namedtuple
 
 
@@ -46,17 +47,26 @@ def get_calibration(eng, sensor):
     return dict(result.fetchall())
 
 
-def get_time_range(eng, table_name):
+def get_time_range(eng, table):
     """
-    Return a tuple of the start and end times (in seconds since 1/1/1970 UTC)
-    for the named SQL table.
+    Return a tuple of the start and end times (in microseconds since
+    1/1/1970 UTC) for the named SQL table.
+
+    :param eng: SQLAlchemy database engine
+    :param table: SQL table name
     """
     meta = MetaData()
-    tbl = Table(table_name, meta, autoload=True, autoload_with=eng)
-    s = select([func.min(tbl.c.timestamp), func.max(tbl.c.timestamp)])
-    conn = eng.connect()
-    t0, t1 = conn.execute(s).fetchone()
-    return t0//1000000, (t1 + 1000000)//1000000
+    try:
+        tbl = Table(table, meta, autoload=True, autoload_with=eng)
+    except NoSuchTableError:
+        t0, t1 = 0, 0
+    else:
+        s = select([func.min(tbl.c.timestamp), func.max(tbl.c.timestamp)])
+        conn = eng.connect()
+        t0, t1 = conn.execute(s).fetchone()
+        if t0 is None:
+            t0, t1 = 0, 0
+    return t0, t1
 
 
 def get_dataset(eng, table, t_start=0, t_end=0):
@@ -67,7 +77,7 @@ def get_dataset(eng, table, t_start=0, t_end=0):
 
     :param eng: SQLAlchemy database engine
     :param table: SQL table name
-    :param t_start: start time (in seconds since 1/1/1970 UTC)
+    :param t_start: start time (in microseconds since 1/1/1970 UTC)
     :param t_end: end time
     :rtype: :class:`pandas.DataFrame`
     """
@@ -76,10 +86,10 @@ def get_dataset(eng, table, t_start=0, t_end=0):
         params = None
     elif t_end == 0:
         query = 'select * from {} where timestamp > ?'.format(table)
-        params = (t_start*1000000,)
+        params = (t_start,)
     else:
         query = 'select * from {} where timestamp between ? and ?'.format(table)
-        params=(t_start*1000000, t_end*1000000)
+        params=(t_start, t_end)
     return pd.read_sql_query(query, eng, params=params)
 
 
